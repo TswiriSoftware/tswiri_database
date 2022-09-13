337eb8d5e8159b821b954a962580640d65ff6453
Werner <werner@scholtzonline.net>
2022-09-13 16:16:15 +0200

    Photo display updates

--- lib/widgets/photo/photo_display.dart
+++ lib/widgets/photo/photo_display.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:tswiri_database/widgets/ml_label_paint/ml_label_paint.dart';
 
 class PhotoDisplay extends StatelessWidget {
   const PhotoDisplay({
@@ -8,8 +7,6 @@ class PhotoDisplay extends StatelessWidget {
     required this.photoPath,
     this.height,
     this.width,
-    this.mlLabelPaint,
-    this.photoNavigation,
   }) : super(key: key);
 
   ///File path of the photo.
@@ -21,38 +18,13 @@ class PhotoDisplay extends StatelessWidget {
   ///The height of the displayed photo.
   final double? height;
 
-  ///MLLabelPaint. required height and width.
-  final MLLabelPaint? mlLabelPaint;
-
-  ///Photo Navigation Widget
-  final Widget? photoNavigation;
-
   @override
   Widget build(BuildContext context) {
-    if (mlLabelPaint == null) {
-      return Image.file(
-        File(photoPath),
-        fit: BoxFit.scaleDown,
-        height: height,
-        width: width,
-      );
-    } else {
-      return SizedBox(
-        height: height ??
-            0 / (width ?? 1 / (MediaQuery.of(context).size.width - 16)),
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          fit: StackFit.expand,
-          children: [
-            Image.file(
-              File(photoPath),
-              fit: BoxFit.scaleDown,
-            ),
-            mlLabelPaint!,
-            photoNavigation ?? const SizedBox.shrink(),
-          ],
-        ),
-      );
-    }
+    return Image.file(
+      File(photoPath),
+      fit: BoxFit.scaleDown,
+      height: height,
+      width: width,
+    );
   }
 }
