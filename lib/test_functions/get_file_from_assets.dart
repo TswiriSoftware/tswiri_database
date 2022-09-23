952c5a40abb8547d7ad0d23de029624bc59aefe8
Werner <werner@scholtzonline.net>
2022-09-23 15:17:24 +0200

    getFileFromAssets update

--- lib/test_functions/get_file_from_assets.dart
+++ lib/test_functions/get_file_from_assets.dart
@@ -6,7 +6,8 @@ import 'package:path_provider/path_provider.dart';
 Future<File> getFileFromAssets(String path) async {
   final byteData = await rootBundle.load('assets/$path');
 
-  final file = File('${(await getTemporaryDirectory()).path}/$path');
+  final file =
+      File('${(await getTemporaryDirectory()).path}/${path.split('/').last}');
   await file.writeAsBytes(byteData.buffer
       .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
 
