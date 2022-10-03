f5f291c07484c6fc608d2b8095b1614dedb9696d
049er <werner@scholtzonline.net>
2022-10-03 16:01:35 +0200

    Changes

--- lib/tswiri_database.dart
+++ lib/tswiri_database.dart
@@ -3,6 +3,9 @@ library tswiri_database;
 import 'dart:io';
 
 import 'package:isar/isar.dart';
+export 'package:tswiri_database/functions/backup/backup_restore_functions.dart';
+export 'package:tswiri_database/functions/backup/create_backup.dart';
+export 'package:tswiri_database/functions/backup/restore_backup.dart';
 
 ///Isar directory.
 Directory? isarDirectory;
