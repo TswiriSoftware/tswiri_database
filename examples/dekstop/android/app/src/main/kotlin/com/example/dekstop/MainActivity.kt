0f137280e632176e7571eb208b4c7854e270d1cc
049er <werner@scholtzonline.net>
2022-10-25 08:19:18 +0200

    Removed OLD examples

--- examples/dekstop/android/app/src/main/kotlin/com/example/dekstop/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dekstop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- examples/old/database_tester/android/app/src/main/kotlin/com/example/database_tester/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.database_tester
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
