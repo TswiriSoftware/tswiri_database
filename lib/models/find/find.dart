fe3eae78f7134805836cb7702369285af3254a1c
049er <werner@scholtzonline.net>
2022-11-01 12:07:09 +0200

    Find Notifier

--- lib/models/find/find.dart
+++ lib/models/find/find.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 
 ///This class finds what the user is looking for :D.
 class Find extends ChangeNotifier {
+  ///A list of enabled filters
+  List<String> enabledFilters = [
+    'Tags',
+    'ML Labels',
+    'Photo Labels',
+    'Object Labels',
+    'Name',
+    'Description',
+    'ML Text',
+  ];
+
   ///A list of the different filters and their purpose.
   final Map<String, String> _findFilters = {
     'Tags': 'Search by container tags',
@@ -12,4 +23,33 @@ class Find extends ChangeNotifier {
     'Description': 'Search by container Description',
     'ML Text': 'Search by detected text',
   };
+
+  ///Get the filters with descriptions.
+  Map<String, String> get findFilters {
+    return _findFilters;
+  }
+
+  ///Get the filternames.
+  List<String> get filters {
+    return _findFilters.keys.toList();
+  }
+
+  ///Toggle a filters state.
+  void toggleFilter(String filter) {
+    if (enabledFilters.contains(filter)) {
+      enabledFilters.removeWhere((element) => element == filter);
+    } else {
+      enabledFilters.add(filter);
+    }
+    notifyListeners();
+  }
+
+  ///Check if a filter is active.
+  bool isFilterActive(String filter) {
+    if (enabledFilters.contains(filter)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
